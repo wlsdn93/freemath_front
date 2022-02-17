6c524d567616681485d0a3d3916467fc876e8377
wlsdn93 <84765181+wlsdn93@users.noreply.github.com>
2022-02-17 22:15:56 +0900

    trivial fix

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="app">
-    <div v-if="!$route.path.includes('/problems/')" class="common-navbar">
-      <CommonNavBar/>
+    <div v-if="$route.path === '/'">
+      <welcome></welcome>
     </div>
-    <div class="content">
-      <router-view></router-view>
+    <div v-if="$route.path !== '/'">
+      <div v-if="!$route.path.includes('/problems/') && !$route.path.includes('/callback/')" class="common-navbar">
+        <CommonNavBar/>
+      </div>
+      <div class="content">
+        <router-view></router-view>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import CommonNavBar from './components/layout/CommonNavBar.vue'
+import Welcome from "@/views/Welcome";
 
 export default {
   name: 'App',
   components: {
     CommonNavBar,
+    Welcome
   }
 }
 </script>
